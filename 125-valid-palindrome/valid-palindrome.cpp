a8eb2df4a081e799412a5b12af7dbe371a290af6
Bhoi Aniruddh <aniruddhbhoi2005@gmail.com>
2026-07-24 22:42:13 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (54.82%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(),s.end(),s.begin(),[](unsigned char c){
+            return tolower(c);
+        });
+
+        s.erase(remove_if(s.begin(),s.end(),[](unsigned char c){
+            return !isalnum(c);}),s.end());
+
+        bool ans = true ;
+        int l = 0 ;
+        int r = s.size()-1;
+
+        while(l<r){
+            if(s[l] != s[r]){
+                ans = false ;
+                break ;
+            }
+            l++;
+            r--;
+        }
+
+    return ans;
+    }   
+};
